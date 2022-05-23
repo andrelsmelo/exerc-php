4c30cad3131aa16bf937f8cea36dea2e78a9f30d
André Melo <andrelucassmelo@gmail.com>
2022-05-23 16:17:13 -0300

    Inserido app.css

--- controllers/persons.php
+++ controllers/persons.php
@@ -2,21 +2,26 @@
 
 
 $person = [
-        "firstName" => "firstNameTest",
-        "lastName" => "lastNameTest",
-        "cpf" => 00000000000,
-        "phone" => 00000000000,
-        "cep" => 00000000,
-        "city" => "cityTest",
-        "district" => "districtTest",
-        "address" => "adressTest",
-        "number" => 00,
-        "complement" => "complementTest",
-        "momFirstName" => "momFirstNameTest",
-        "momLastName" => "momLastNameTest",
-        "dadFirstName" => "dadFirstNameTest",
-        "dadLastName" => "dadLastNameTest",
-        "IR2022" => 00000,
-        "IR2021" => 00000,
-        "IR2020" => 0000
+        "firstName" => $_POST["firstName"],
+        "lastName" => $_POST["lastName"],
+        "cpf" => $_POST["cpf"],
+        "phone" => $_POST["phone"],
+        "cep" => $_POST["cep"],
+        "city" => $_POST["city"],
+        "district" => $_POST["district"],
+        "address" => $_POST["address"],
+        "number" => $_POST["number"],
+        "complement" => $_POST["complement"],
+        "momFirstName" => $_POST["momFirstName"],
+        "momLastName" =>  $_POST["momLastName"],
+        "dadFirstName" => $_POST["dadFirstName"],
+        "dadLastName" => $_POST["dadLastName"],
+        "IR2022" => $_POST["IR2022"],
+        "IR2021" => $_POST["IR2021"],
+        "IR2020" => $_POST["IR2020"]
     ];
+
+    function showPerson() {
+        print_r("Passou");
+        return $person;
+    }
